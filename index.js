6205125d73aaa9871fe8c2639b4ba538e3b70136
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-27 16:24:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+ 
+ function moveDodgerRight(){
+   var rightNumbers= dodger.style.right.replace('px','')
+   var right = parseInt(rightNumbers, 10)
+   if (right > 0){
+     dodger.style.right=`${right + 1}px;`}
+   } 
