253278fe787a199ccc553d56b906e9dadbe2f6db
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-27 16:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,5 @@
    var rightNumbers= dodger.style.right.replace('px','')
    var right = parseInt(rightNumbers, 10)
    if (right > 0){
-     dodger.style.right=`${right + 1}px;`
+     dodger.style.right=`${right + 1}px;`}
    } 
-   
- })
